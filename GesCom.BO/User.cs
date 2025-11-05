fd32c79be9e3476b6369b8f294a1f8f000b5f905
ANDRE Antoine <totoandre2507@gmail.com>
2025-11-05 12:21:49 +0100

    Creation Class User

--- GesCom.BO/User.cs
+++ GesCom.BO/User.cs
@@ -8,5 +8,33 @@ namespace GesCom.BO
 {
     public class User
     {
+        private int id;
+        private string login;
+        private string password;
+
+        public User(int id, string login, string password)
+        {
+            this.id = id;
+            this.login = login;
+            this.password = password;
+        }
+
+        public int id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string login
+        {
+            get { return this.login; }
+            set { this.login = value; }
+        }
+
+        public string password
+        {
+            get { return this.password; }
+            set { this.password = value; }
+        }
     }
 }
