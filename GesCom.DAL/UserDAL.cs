99e1e158c010b78c8cd1f1a2c998ef0e80467458
Diegoodlvv <deoliveiradiego953@gmail.com>
2025-11-05 12:06:22 +0100

    création userDAL

--- GesCom.DAL/UserDAL.cs
+++ GesCom.DAL/UserDAL.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace GesCom.DAL
 {
-    internal class UserDAL
+    public class UserDAL
     {
+
+        private static UserDAL unUserDal;
+        
+        public static UserDAL GetUnUserDAL()
+        {
+            if(unUserDal == null)
+            {
+                unUserDal = new UserDAL();
+            }
+
+            return unUserDal
+        }
+
+        public List<Users> GetListUsers()
+        {
+            List<Users> users = new List<Users>();
+            string query = "SELECT * FROM USERS";
+
+            using (SqlConnection connexion = ConnexionDB.GetConnexionDB().GetSqlConnexion())
+            {
+                SqlCommand cmd = new SqlCommand(query, connexion);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    User user = new User(
+                        reader.GetInt32(),
+                        reader.GetString(),
+                        reader.GetString()
+                        );
+                    users.Add(user);
+                }
+                reader.Close();
+            }
+
+            return users;
+        }
     }
 }
